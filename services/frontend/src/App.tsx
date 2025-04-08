3df229ec2e278550c6cec9db002051c5878653ff
Schalmanoff <yaribakal@gmail.com>
2025-04-08 12:25:26 +0300

    applied global styles

--- services/frontend/src/App.tsx
+++ services/frontend/src/App.tsx
@@ -1,5 +1,17 @@
 function App() {
-	return <></>;
+  return (
+    <>
+      <h3>schematico</h3>
+      <h1>This is h1</h1>
+      <h2>This is h2</h2>
+      <h3>This is h3</h3>
+      <p>This is p</p>
+      <a>This is a</a>
+      <a>
+        <h3>This is h1</h3>
+      </a>
+    </>
+  );
 }
 
 export default App;
