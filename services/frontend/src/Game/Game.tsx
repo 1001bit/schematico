33f3715be8b6148df92c27b048d564993d4b0a44
Schalmanoff <yaribakal@gmail.com>
2025-04-11 12:01:03 +0300

    made zoom

--- services/frontend/src/Game/Game.tsx
+++ services/frontend/src/Game/Game.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Layer, Stage } from "react-konva";
+import { Layer, Rect, Stage } from "react-konva";
 import GridLines from "./Grid";
 import { KonvaEventObject, Node, NodeConfig } from "konva/lib/Node";
 import { ToolType } from "../components/Toolbar/Toolbar";
@@ -12,7 +12,7 @@ export function Game(props: GameProps) {
   const [width, setWidth] = useState(window.innerWidth);
   const [height, setHeight] = useState(window.innerHeight);
 
-  const [scale, _setScale] = useState(1);
+  const [scale, setScale] = useState(1);
 
   const [pos, setPos] = useState({ x: 0, y: 0 });
 
@@ -38,10 +38,34 @@ export function Game(props: GameProps) {
 
   function handleDragMove(e: KonvaEventObject<DragEvent, Node<NodeConfig>>) {
     const node = e.target;
-    const x = -node.x();
-    const y = -node.y();
+    const x = node.x();
+    const y = node.y();
 
     setPos({ x, y });
+  }
+
+  function handleWheel(e: KonvaEventObject<WheelEvent, Node<NodeConfig>>) {
+    e.evt.preventDefault();
+    const oldScale = scale;
+    const ampl = Math.abs(e.evt.deltaY / 100);
+    const scaleBy = 1.1 * ampl;
+    const pointer = e.target.getStage()?.getPointerPosition();
+    if (!pointer) return;
+
+    const zoomTo = {
+      x: (-pos.x + pointer.x) / oldScale,
+      y: (-pos.x + pointer.y) / oldScale,
+    };
+
+    const newScale = e.evt.deltaY < 0 ? oldScale * scaleBy : oldScale / scaleBy;
+    setScale(newScale);
+
+    const newPos = {
+      x: -(zoomTo.x - pointer.x / newScale) * newScale,
+      y: -(zoomTo.y - pointer.y / newScale) * newScale,
+    };
+
+    setPos(newPos);
   }
 
   return (
@@ -58,17 +82,20 @@ export function Game(props: GameProps) {
         onDragStart={(_e) => setDraggingState(true)}
         onDragMove={(e) => handleDragMove(e)}
         onDragEnd={(_e) => setDraggingState(false)}
-        on
+        onWheel={(e) => handleWheel(e)}
         scaleX={scale}
         scaleY={scale}
+        x={pos.x}
+        y={pos.y}
       >
         <Layer>
+          <Rect x={10} y={10} width={100} height={100} fill={"red"}></Rect>
           <GridLines
             width={width / scale}
             height={height / scale}
             tile={30}
-            camX={pos.x / scale}
-            camY={pos.y / scale}
+            camX={-pos.x / scale}
+            camY={-pos.y / scale}
           ></GridLines>
         </Layer>
       </Stage>
