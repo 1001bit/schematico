f2db179edc01d4f72565d95c6bffea79341a4921
Schalmanoff <yaribakal@gmail.com>
2025-04-28 15:14:20 +0300

    removed unused stuff

--- unused/shared/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/1001bit/schematico/shared
-
-go 1.24.1
-
-require (
-	github.com/golang-jwt/jwt/v5 v5.2.2
-	github.com/lib/pq v1.10.9
-)
--- unused/sql/user-init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users
-(
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(32) NOT NULL,
-    passhash VARCHAR(60) NOT NULL
-);
-
-CREATE UNIQUE INDEX users_username_lower_idx ON users (LOWER(username));
