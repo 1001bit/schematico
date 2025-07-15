8ab57b096127b7fd2c33f9cce8bf17f1a59f565e
Schalmanoff <yaribakal@gmail.com>
2025-07-15 12:07:40 +0300

    refactor PublishProject and SaveProject to use transactions for database operations

--- services/project/projectmodel/publish.go
+++ services/project/projectmodel/publish.go
@@ -3,12 +3,25 @@ package projectmodel
 import "context"
 
 func (s *ProjectStorage) PublishProject(ctx context.Context, name string, tileMapStr string) (string, error) {
-	newId := ""
-	err := s.db.QueryRowContext(ctx, "INSERT INTO projects (name) VALUES ($1) RETURNING id", name).Scan(&newId)
+	tx, err := s.db.BeginTx(ctx, nil)
 	if err != nil {
 		return "", err
 	}
-	_, err = s.db.ExecContext(ctx, "INSERT INTO project_maps (id, map) VALUES ($1, $2::jsonb)", newId, tileMapStr)
+	defer tx.Rollback()
 
-	return newId, err
+	newId := ""
+	err = tx.QueryRowContext(ctx, "INSERT INTO projects (name) VALUES ($1) RETURNING id", name).Scan(&newId)
+	if err != nil {
+		return "", err
+	}
+	_, err = tx.ExecContext(ctx, "INSERT INTO project_maps (id, map) VALUES ($1, $2::jsonb)", newId, tileMapStr)
+	if err != nil {
+		return "", err
+	}
+
+	if err = tx.Commit(); err != nil {
+		return "", err
+	}
+
+	return newId, nil
 }
--- services/project/projectmodel/save.go
+++ services/project/projectmodel/save.go
@@ -3,11 +3,24 @@ package projectmodel
 import "context"
 
 func (s *ProjectStorage) SaveProject(ctx context.Context, id string, name string, tileMapStr string) error {
-	_, err := s.db.ExecContext(ctx, "UPDATE projects SET name=$1 WHERE id=$2", name, id)
+	tx, err := s.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
 	}
-	_, err = s.db.ExecContext(ctx, "UPDATE project_maps SET map=$1::jsonb WHERE id=$2", tileMapStr, id)
+	defer tx.Rollback()
 
-	return err
+	_, err = tx.ExecContext(ctx, "UPDATE projects SET name=$1 WHERE id=$2", name, id)
+	if err != nil {
+		return err
+	}
+	_, err = tx.ExecContext(ctx, "UPDATE project_maps SET map=$1::jsonb WHERE id=$2", tileMapStr, id)
+	if err != nil {
+		return err
+	}
+
+	if err = tx.Commit(); err != nil {
+		return err
+	}
+
+	return nil
 }
